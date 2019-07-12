d9fa3e864e9d803d6133f27ec01b630c232df28a
kwoneyng <nan308@naver.com>
2019-07-12 16:39:18 +0900

    I got it

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, escape, request
 from decouple import config
 import pprint
 import requests
-
 app = Flask(__name__)
 
+API_TOKEN = config('API_TOKEN')
+NAVER_CLIENT_ID = config('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = config('NAVER_CLIENT_SECRET')
 
 API_TOKEN = config('API_TOKEN')
 
@@ -27,11 +29,38 @@ def telegram():
         chat_id = from_telegram.get('message').get('chat').get('id')
         text = from_telegram.get('message').get('text')
 
-        if text == '점심메뉴' :
-            text = '짜장면이나 먹어'
+        # 처음 네글자가 '/번역' 일 때        
+        if text[0:4] == '/한영 ':
+            #요청에 대한 정보가 저장되어있다.
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,
+            }
+            data = {
+                'source': 'ko',
+                'target': 'en',
+                'text': text[4:]
+            }
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText')
+            
 
-        print('chat_id : ',chat_id)
-        print('text : ',text)
+        if text[0:4] == '/영한 ':
+            #요청에 대한 정보가 저장되어있다.
+            headers = {
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,
+            }
+            data = {
+                'source': 'en',
+                'target': 'ko',
+                'text': text[4:]
+            }
+
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            text = papago_res.json().get('message').get('result').get('translatedText')
 
     base_url = 'https://api.telegram.org'
     token = config('API_TOKEN')
@@ -40,9 +69,6 @@ def telegram():
 
     response = requests.get(api_url)
     
-
-
-
     return '', 200 # ,뒤의 값 status code
 
 
